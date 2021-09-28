49f04a30f8937d24cbfbf827e1afe80bf01c78f0
Xiang Wang <ramwin@qq.com>
2021-09-28 22:55:04 +0800

    结构体

---
+++ 结构体/base.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+struct Person {
+    char name[30];
+    int age;
+    int id;
+};
+
+int main() {
+    struct Person person = {"alice", 18, 1};
+    // printf(person.name);
+    return 0;
+};
