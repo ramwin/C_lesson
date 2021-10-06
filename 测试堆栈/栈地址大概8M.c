9138f1862ff65b19f27db60a4bc809b31b7b0b9e
Xiang Wang <ramwin@qq.com>
2021-10-06 23:19:36 +0800

    栈地址

---
+++ 测试堆栈/栈地址大概8M.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int f(int x) {
+    int p;
+    printf("调用f, x(%d)内存地址: %d\n", x, &x);
+    if (x == 1 or x == 2) {
+        return 1;
+    }
+    return f(x-1) + x;
+};
+
+int main() {
+    int x = 262000;  // 这个就溢出, segmentation fault了
+    int x = 260000;  // 这个就不会溢出
+    printf("f(%d)是: %d\n", x, f(x));
+}
