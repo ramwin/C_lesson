36b53be602df290a0982c10a39ab9165754a7198
Xiang Wang <ramwin@qq.com>
2021-12-18 22:49:26 +0800

    空命名空间

---
+++ c++/命名空间/空命名空间.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace {
+  using namespace std;
+  void func() {
+    cout << "恭喜你调用我了\n";
+  }
+};
+
+// void func() {
+//     cout << "恭喜你外部了\n";
+// };
---
+++ c++/命名空间/空命名空间_main.cpp
@@ -0,0 +1,4 @@
+void func();  // 只能调用空命名空间里面外部的func
+int main() {
+  func();
+};
