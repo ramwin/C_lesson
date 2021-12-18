c3a046d98a5534350fd093a2da3575e62364f461
Xiang Wang <ramwin@qq.com>
2021-12-18 22:05:03 +0800

    整理

--- huchi2.c
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "第二个胡吃" <<endl;
-    return 0;
-}
--- huchishabi.c
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout <<"胡持"<<endl;
-    return 0;
-}
