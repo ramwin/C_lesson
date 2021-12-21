361c5bd8ad86c1a796e7d4d08dea25d0dd7250e9
Xiang Wang <ramwin@qq.com>
2021-12-21 22:22:02 +0800

    PrintInstruction

---
+++ llvm/PrintInstruction/input.c
@@ -0,0 +1,10 @@
+int square(int x) {
+    return x * 7;
+};
+
+int main() {
+    int b = 14;
+    int c = b + 1;
+    // b += square(a);
+    return c;
+}
---
+++ llvm/PrintInstruction/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Xiang Wang(ramwin@qq.com)
+
+set -e
+
+if [ ! -d "build" ];
+then
+    mkdir build
+fi
+cd build
+cmake ../
+cmake --build .
+clang -O0 -Xclang -disable-O0-optnone -S -emit-llvm ../input.c -o ../input.ll
+opt -load-pass-plugin ./libPrintInstruction.so -passes=print-instruction ../input.ll -o ../optimized.ll
+lli ../optimized.ll
