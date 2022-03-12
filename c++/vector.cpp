1df5e434defba8f1617131e8e5f272bd27e4154a
Xiang Wang <ramwin@qq.com>
2022-03-12 22:09:58 +0800

    vector

---
+++ c++/vector.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+
+int main() {
+    std::vector<int> a;
+    a.push_back(3);
+    a.push_back(4);
+    std::cout << a[0];
+    std::cout << a[1];
+};
---
+++ llvm/GettingStarted/hello.s
@@ -0,0 +1,28 @@
+	.text
+	.file	"hello.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:                                # %entry
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	movl	$.Lstr, %edi
+	callq	puts@PLT
+	xorl	%eax, %eax
+	popq	%rcx
+	.cfi_def_cfa_offset 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.Lstr,@object                   # @str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.Lstr:
+	.asciz	"hello world"
+	.size	.Lstr, 12
+
+	.ident	"clang version 14.0.0 (git@github.com:llvm/llvm-project.git 69ba522c58d0cf43097a08087b1b31d6edb46706)"
+	.section	".note.GNU-stack","",@progbits
