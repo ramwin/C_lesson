de74c77ae2c70e4309563443b7d4b12c06c37156
Xiang Wang <ramwin@qq.com>
2024-05-19 22:04:08 +0800

    输入输出判断

--- c++/输入输出.cpp
+++ c++/输入输出.cpp
@@ -6,12 +6,21 @@ void test_sscan() {
   int a;
   float b;
   char c[10];
-  std::sscanf(input, "%d%f%9s", &a, &b, &c);
+  std::sscanf(input, "%d%f%9s", &a, &b, c);
   cout << "a = " << a << endl;
   cout << "b = " << b << endl;
   cout << "c = " << c << endl;
 };
 
+void test_cin() {
+  int a;
+  while (cin >> a) {
+    cout << "输入了" << a << endl;
+  };
+  cout << "结束输入了\n";
+};
+
 int main() {
-  test_sscan();
+  test_cin();
+  // test_sscan();
 }
